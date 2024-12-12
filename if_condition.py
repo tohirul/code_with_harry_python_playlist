30dd26745862e4ebd4bb4d36f59a28e1bb601bbb
tohirul <tohirul.islam164@gmail.com>
2024-12-13 02:38:18 +0600

    [Date] Dec 13, 2024 02:37 [Update] if_condition.py [Complete] Usage of if else condition

---
+++ if_condition.py
@@ -0,0 +1,19 @@
+# print("Welcome to the age checker\n")
+
+# user_age = int(input("Enter your age: "))
+# print("Your age is:", user_age)
+# if user_age >= 18:
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")
+
+
+print("Is it raining outside?")
+is_raining = 1 if input("(True: 1/False: 0): ") == "True" else 0
+
+# print(is_raining)
+
+if (is_raining):
+    print("It is raining")
+else:
+    print("It is not raining")
