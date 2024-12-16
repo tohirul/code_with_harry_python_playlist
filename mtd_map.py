bf21953f2d77a1b46f5970fa0905fbec47800d05
tohirul <tohirul.islam164@gmail.com>
2024-12-17 01:48:09 +0600

    [Date] Dec 17, 2024 01:47 [Update] mtd_map.py [Complete] Usecase map on List

---
+++ mtd_map.py
@@ -0,0 +1,43 @@
+
+# random marks between 70 - 100
+
+marks = [75, 93, 99, 89, 81, 74, 84]
+
+
+def divide_map(x):
+    return x / 2
+
+
+# marks_list = [divide_map(mark) for mark in marks]
+marks_list = list(map(divide_map, marks))
+print(marks_list)
+
+
+# write some documents of users
+user_list = [
+    {
+        "name": "John",
+        "age": 23,
+        "gender": "male"
+    },
+    {
+        "name": "Jane",
+        "age": 25,
+        "gender": "female"
+    },
+    {
+        "name": "Jack",
+        "age": 27,
+        "gender": "male"
+    }
+]
+
+
+def write_messge(user):
+    print(f"Hello {user['name']}, your age is {user['age']}.")
+
+
+# for user in user_list:
+#     write_messge(user)
+
+list(map(write_messge, user_list))
